fa03c36e379dc8ec7a2d50b4e050e66f892a2b88
Miles Pines <miles542@live.com>
2022-11-14 00:11:36 -0500

    feat: file

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,11 +1,52 @@
 import pandas as pd
+import os
 from datetime import datetime
 
-df = pd.read_csv('Electronics.csv')
+folder = 'bulk'
 
-def updateData(row):
-    return datetime.utcfromtimestamp(int(row)).strftime('%Y-%m-%d')
+workingDirectory = os.getcwd() + "/" + folder
 
-df["timestamp"] = df["timestamp"].apply(updateData)
+FRACTION = 0.02
 
-df.to_csv('Electronics.csv')
+print("Generating File List!")
+
+# Generate List of All Bulk CSV Files
+f = []
+for (dirpath, dirnames, filenames) in os.walk(workingDirectory):
+    for innerFile in filenames:
+        if innerFile.endswith(".json"):
+            f.append(innerFile)
+    break
+
+# Run Script for every file in list
+df_list = []
+
+for count, filename in enumerate(f, start=1):
+    print("Modifying " + filename + ": " + str(count) + "/" + str(len(f)))
+
+    tempPath = './' + folder + "/" + filename
+    
+    # Load Data into Dataframe
+    df = pd.read_json(tempPath, lines=True)
+
+    # Select Random Percentage of Data
+    df = df.sample(frac=FRACTION)
+
+    # Create Ideal Rows
+    Item_ID = (df['asin']).tolist()
+
+    Rating = (df['overall']).tolist()
+
+    Timestamp = [datetime.strptime(x, "%m %d, %Y").strftime('%Y-%m-%d') for x in df['reviewTime']]
+
+    Num_Words = [len(str(x).split()) for x in df['reviewText']]
+
+    Category = [(filename.replace("5", "").replace("_", " ")[:-6])] * len(Num_Words)
+
+    df = pd.DataFrame({'Item_ID':Item_ID, 'Rating':Rating, 'Timestamp':Timestamp, 'Num_Words':Num_Words, 'Category':Category})
+
+    df_list.append(df)
+
+df = pd.concat(df_list)
+
+df.to_csv('out.csv')
