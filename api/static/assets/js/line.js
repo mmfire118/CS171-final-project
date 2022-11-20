053439b331dc9e55daa4aacd6e20521dbae52707
Sam Perlman <sperlman@college.harvard.edu>
2022-11-20 13:19:49 -0500

    added axis text

--- api/static/assets/js/line.js
+++ api/static/assets/js/line.js
@@ -1,10 +1,11 @@
 class LineVis {
-    constructor(parentElement, dropdownId, titleText, rollupFunction, data){
+    constructor(parentElement, dropdownId, titleText, yAxisText, rollupFunction, data){
         this.parentElement = parentElement;
         this.dropdownId = dropdownId
         this.data = data;
         this.rollupFunction = rollupFunction;
         this.titleText = titleText;
+        this.yAxisText = yAxisText;
 
         // date methods
         this.parseDate = d3.timeParse("%Y-%m-%d");
@@ -16,7 +17,7 @@ class LineVis {
     initVis(){
         let vis = this;
 
-        vis.margin = {top: 20, right: 20, bottom: 40, left: 40};
+        vis.margin = {top: 20, right: 20, bottom: 50, left: 60};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
@@ -36,6 +37,22 @@ class LineVis {
                 `translate(${vis.width / 2}, 10)` : `translate(${vis.width / 2}, ${vis.height-40})`)
             .attr('text-anchor', 'middle')
             .attr("fill", "white");
+
+        // add x axis text
+        vis.svg.append('g')
+            .attr('class', 'axis-text')
+            .append('text')
+            .text("Timestamp")
+            .attr('transform', `translate(${vis.width / 2}, ${vis.height+(vis.margin.bottom * 0.7)})`)
+            .attr('text-anchor', 'middle');
+
+        // add y axis text
+        vis.svg.append('g')
+            .attr('class', 'axis-text')
+            .append('text')
+            .text(vis.yAxisText)
+            .attr('transform', `translate(-35,${vis.height/2})rotate(-90)`)
+            .attr('text-anchor', 'middle');
 
         /* tooltip
         vis.tooltip = d3.select("body").append('div')
